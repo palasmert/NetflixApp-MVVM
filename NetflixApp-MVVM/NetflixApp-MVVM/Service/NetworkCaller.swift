2487472f09245c861cb03fbc89fefaac62c820bb
palasmert <mertpalas39@gmail.com>
2024-11-15 17:16:28 +0300

    service

---
+++ NetflixApp-MVVM/NetflixApp-MVVM/Service/NetworkCaller.swift
@@ -0,0 +1,42 @@
+//
+//  NetworkCaller.swift
+//  NetflixApp-MVVM
+//
+//  Created by Mert Palas on 15.11.2024.
+//
+
+import Foundation
+
+protocol NetworkServiceProtocol {
+    func fetchData<T: Decodable>(_ endPoint: EndPoint, completion: @escaping (Result<T,Error>) -> Void)
+}
+
+final class NetworkCaller {
+    
+    func fetchData<T: Decodable>(_ endPoint: EndPoint, completion: @escaping (Result<T,Error>) -> Void) {
+        let task = URLSession.shared.dataTask(with: endPoint.request()) { data, response, error in
+            
+            if let error =  error {
+                completion(.failure(error))
+            }
+            
+            guard let response = response as? HTTPURLResponse , response.statusCode >= 200 , response.statusCode <= 299 else {
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(URLError(.badURL)))
+                return
+            }
+            
+            do {
+                let decoderData = try JSONDecoder().decode(T.self, from: data)
+                    completion(.success(decoderData))
+            } catch let error {
+                print(error)
+                
+            }
+        }
+        task.resume()
+    }
+}
---
+++ NetflixApp-MVVM/NetflixApp-MVVM/Service/NetworkConstants.swift
@@ -0,0 +1,81 @@
+//
+//  NetworkConstants.swift
+//  NetflixApp-MVVM
+//
+//  Created by Mert Palas on 15.11.2024.
+//
+
+import Foundation
+//https://api.themoviedb.org/3/movie/popular?api_key=fab6464769e3d822047ea1ffd1e6430e
+
+protocol EndPointProtocol {
+    var baseURL: String {get}
+    var genreURL: String {get}
+    var apiKey: String {get}
+    var method: HTTPMethod{get}
+    
+    func movieApiURL() -> String
+    func request() -> URLRequest
+}
+
+enum HTTPMethod: String {
+    case post = "POST"
+    case get = "GET"
+}
+
+enum EndPoint {
+    case popular
+    case topRated
+    case upComing
+}
+
+extension EndPoint: EndPointProtocol {
+    var baseURL: String {
+        return "https://api.themoviedb.org/3/movie"
+    }
+    
+    var genreURL: String {
+        switch self {
+        case.popular:
+            return "popular"
+        case.topRated:
+            return "top_rated"
+        case.upComing:
+            return "upcoming"
+        }
+    }
+    
+    var apiKey: String {
+        return "api_key=fab6464769e3d822047ea1ffd1e6430e"
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case.popular:
+            return .get
+        case.topRated:
+            return .get
+        case.upComing:
+            return .get
+        }
+    }
+    
+    func movieApiURL() -> String {
+        return "\(baseURL)\(genreURL)\(apiKey)"
+    }
+    func request() -> URLRequest {
+        guard let apiURL = URLComponents(string: movieApiURL()) else {
+            fatalError("url comp oluşturulamadı")
+        }
+        
+        guard let url = apiURL.url else {
+            fatalError("url oluşturulamadı")
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        
+        return request
+        
+    }
+}
